86faef1ce20e638957cb1b4cdc6eb43bddac420d
Ryan <nolanryan2016@gmail.com>
2023-10-28 19:58:08 -0400

    first commit

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+
+
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function (req, res, next) {
+    res.render('index', {
+        title: 'MSAL Node & Express Web App',
+        isAuthenticated: req.session.isAuthenticated,
+        username: req.session.account?.username,
+    });
+});
+
+module.exports = router;
